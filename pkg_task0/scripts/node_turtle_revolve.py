0f8af096658b2dd6b8b95ed3da15907550ac0d4c
Shivam Malviya <shivammalviya71234@gmail.com>
2020-10-18 22:06:25 +0530

    Distance from theta

--- pkg_task0/scripts/node_turtle_revolve.py
+++ pkg_task0/scripts/node_turtle_revolve.py
@@ -1,71 +1,93 @@
 #!/usr/bin/env python
 
-
+# Python libraries
 import math
 import sys
+
+# ROS libraries
 import rospy
+
+# ROS messages
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
-def pose_callback(msg):
-    rospy.loginfo(msg)
-    pass
 
-
-def revolve(linear_x, angular_z):
-    # Declaration
-    rospy.init_node('node_turtle_revolve', anonymous=True)
-    rate = rospy.Rate(10)
-    velocity_publisher = rospy.Publisher(
+class NodeTurtleRevolve:
+    def __init__(self, vx, wz):
+        # turtle1/cmd_vel publisher
+        self.velocity_publisher = rospy.Publisher(
             'turtle1/cmd_vel',
             Twist,
             queue_size=10,
-            latch=True
-    )
-    rospy.Subscriber("/turtle1/pose", Pose, callback=pose_callback)
-    velocity_msg = Twist()
+        )
+        # turtle1/pose subscriber
+        rospy.Subscriber(
+            "/turtle1/pose", 
+            Pose,
+            callback=self.pose_callback
+        )
+        self.rate = rospy.Rate(10)
+        self.velocity_msg = Twist()
+        
+        # Used to calculate the distance covered
+        # in the callback
+        self.theta = 0
+        self.shift = 0
+        
+        # Intialial velocity to published
+        self.velocity_msg.linear.x = vx
+        self.velocity_msg.linear.y = 0
+        self.velocity_msg.linear.z = 0
+        self.velocity_msg.angular.x = 0
+        self.velocity_msg.angular.y = 0
+        self.velocity_msg.angular.z = wz
+        
+        # Calculation of the total distance 
+        # to be covered
+        self.radius = abs(
+            self.velocity_msg.linear.x
+            /self.velocity_msg.angular.z
+        )
+        self.total_distance = 2*math.pi*self.radius
 
-    # Initialisation
-    velocity_msg.linear.x = linear_x
-    velocity_msg.linear.y = 0
-    velocity_msg.linear.z = 0
-    velocity_msg.angular.x = 0
-    velocity_msg.angular.y = 0
-    velocity_msg.angular.z = angular_z
 
-    # Revolution
-    try:
-        T = abs(2*math.pi/velocity_msg.angular.z)
-    except ZeroDivisionError:
-        T = 0
+    def pose_callback(self, position):
+        # Convert theta into 0 to infinity format 
+        dtheta = position.theta + self.shift - self.theta
+        self.shift += (dtheta < -math.pi) * 2*math.pi
+        self.theta = position.theta + self.shift
 
-    total_dist = abs(T * velocity_msg.linear.x)    
-    current_dist = 0
-    t0 = rospy.Time.now().to_sec()
 
-    while (current_dist < total_dist):
-        velocity_publisher.publish(velocity_msg)
-        t1 = rospy.Time.now().to_sec()
-        current_dist = velocity_msg.linear.x * (t1-t0)
-        rospy.loginfo("Current distance = %s" % current_dist)
-        rospy.loginfo("Total distance = %s" % total_dist)
-        rate.sleep() 
+    def revolve(self):
 
-    #rospy.loginfo("Delta t = %s" % (t1-t0))
-    #rospy.loginfo("T = %s" % T)
-    velocity_msg.linear.x = 0
-    velocity_msg.angular.z = 0
-    velocity_publisher.publish(velocity_msg)
-    log_msg = "Goal reached"
-    rospy.loginfo(log_msg)
+        # Revolve till total distance is covered or 
+        # Ctrl+C is pressed.
+        while ((self.theta*self.radius < self.total_distance) 
+                and not rospy.is_shutdown()):
+            self.velocity_publisher.publish(self.velocity_msg)
+            self.rate.sleep()
+            rospy.loginfo(
+                "Distance covered = %s" % (self.theta*self.radius)
+            )
 
-    rospy.spin()
+        # Stop the turtle and display "Goal reached!!" 
+        # if the total distance is covered.
+        if not rospy.is_shutdown():
+            self.velocity_msg.linear.x = 0
+            self.velocity_msg.angular.z = 0
+            self.velocity_publisher.publish(self.velocity_msg)
+            log_msg = "Goal reached!!"
+            rospy.loginfo(log_msg)
 
 
 if __name__ == "__main__":
     args = rospy.myargv(argv=sys.argv)
+    rospy.init_node("node_turtle_revolve", anonymous=True)
+    node_turtle_revolve = NodeTurtleRevolve(
+        float(args[1]),
+        float(args[2]))
     try:
-        revolve(linear_x=float(args[1]),
-                angular_z=float(args[2]))
+        node_turtle_revolve.revolve()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
